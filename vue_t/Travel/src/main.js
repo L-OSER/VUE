2b4751b2ff2978d5b6943e0e609828aad3733998
周嘉俊 <770236524@qq.com>
2018-06-22 17:43:28 +0800

    构建项目结构

--- vue_t/Travel/src/main.js
+++ vue_t/Travel/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/base.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+/* 解决移动端点击三百毫秒的延迟 */
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ vue_t/Travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+   <h1>home</h1>
+   <router-link to="/list">列表页</router-link>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
