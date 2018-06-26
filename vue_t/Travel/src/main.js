7ba880099be9f78f79628ada9d9ca6cf1f85b03b
周嘉俊 <770236524@qq.com>
2018-06-26 14:27:58 +0800

    完成头部组件，配置目录快捷路径

--- vue_t/Travel/src/main.js
+++ vue_t/Travel/src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/base.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 /* 解决移动端点击三百毫秒的延迟 */
