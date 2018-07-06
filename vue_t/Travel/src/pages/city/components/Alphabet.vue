115bfc712f2efb22339347c828df3c8a2a760ac5
周嘉俊 <770236524@qq.com>
2018-07-06 14:50:43 +0800

    perfomance

--- vue_t/Travel/src/pages/city/components/Alphabet.vue
+++ vue_t/Travel/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) in cities" :key="key" v-text="key"></li>
+    <li class="item" v-for="(item, key) in letters"
+        :key="key"
+        v-text="item"
+        :ref="item"
+        @click="handleLetterClick"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+    ></li>
   </ul>
 </template>
 
@@ -9,6 +17,51 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    // 重新渲染的时候，（生命钩子）
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerHTML)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 22)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
