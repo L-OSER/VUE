b34066948d416c69c70f946dceae5a2b23e0e14d
周嘉俊 <770236524@qq.com>
2018-07-11 15:02:59 +0800

    header animation

--- vue_t/Travel/src/pages/detail/Detail.vue
+++ vue_t/Travel/src/pages/detail/Detail.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
+  <div class="detail">
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+    .detail
+      height : 50rem
 </style>
